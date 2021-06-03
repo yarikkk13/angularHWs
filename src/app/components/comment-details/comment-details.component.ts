b18e2b9946e57cee537240362f59e1243b971fb9
yarikkk13 <yaroslav.mahas@gmail.com>
2021-06-03 14:27:42 +0300

    comments ready

---
+++ src/app/components/comment-details/comment-details.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {IComment} from "../../models/Comment";
+import {ActivatedRoute} from "@angular/router";
+import {CommentService} from "../../services/comment.service";
+
+@Component({
+  selector: 'app-comment-details',
+  templateUrl: './comment-details.component.html',
+  styleUrls: ['./comment-details.component.css']
+})
+export class CommentDetailsComponent implements OnInit {
+  comment: IComment;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private commentService: CommentService) {
+    this.activatedRoute.params
+      .subscribe(params => {
+        this.commentService.getComment(params.id)
+          .subscribe(value => this.comment = value)
+      })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
